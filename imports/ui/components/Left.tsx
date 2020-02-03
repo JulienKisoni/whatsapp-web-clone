667d307c621cbee7d75f72b5c8a9fb553d545a6e
JulienKisoni <kisonijulien@gmail.com>
2020-02-03 08:46:43 +0100

    Going on with chats

--- imports/ui/components/Left.tsx
+++ imports/ui/components/Left.tsx
@@ -4,16 +4,19 @@ import { Meteor } from 'meteor/meteor';
 import StyledLeft from '../elements/StyledLeft';
 import Header from './Header';
 import Avatar from './Avatar';
+import Status from './Status';
+import Searchbar from './Searchbar';
 
 const icons:string[] = ["circle-notch", "comment-alt", "ellipsis-v"];
-const avatar_url:string = Meteor.user().profile.picture;
 
 const Left = (props:any):JSX.Element => {
     return (
         <StyledLeft>
             <Header icons={icons} iconClass="greyIcon">
-                <Avatar avatar_url={avatar_url}/>
+                <Avatar avatar_url={Meteor.user().profile.picture}/>
             </Header>
+            <Status />
+            <Searchbar />
         </StyledLeft>
     )
 }
--- server/main.ts
+++ server/main.ts
@@ -1,14 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 
-import { createDummyUsers } from '../imports/api/helpers';
+import { createDummyUsers, createDummyChats } from '../imports/api/helpers';
 import { dummyUsers } from '../imports/api/users';
+import { ChatsCollection, dummyChats } from '../imports/api/chats';
 
 Meteor.startup(() => {
     const numberOfUsers:number = Meteor.users.find().count();
+    const numberOfChats:number = ChatsCollection.find().count();
     if(numberOfUsers === 0) {
         console.log("Il n'ya pas d'utilisateurs");
         createDummyUsers(dummyUsers);
     } else {
         console.log("Il y a des utilisateurs");
+    }
+    if(numberOfChats === 0) {
+        console.log("Il n'ya pas de chats");
+        createDummyChats(dummyChats);
+    } else {
+        console.log("Il y a des Chats");
     }
 });
