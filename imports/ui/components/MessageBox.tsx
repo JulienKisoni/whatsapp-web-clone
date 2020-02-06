172acc61b64daefe2c301cd8e875e7452a380e9b
JulienKisoni <kisonijulien@gmail.com>
2020-02-06 10:42:52 +0100

    Add Day & MessageText

--- imports/ui/components/MessageBox.tsx
+++ imports/ui/components/MessageBox.tsx
@@ -4,6 +4,8 @@ import _ from 'lodash';
 import moment from 'moment';
 
 import StyledMessageBox from '../elements/StyledMessageBox';
+import Day from './Day';
+import MessageText from './MessageText';
 
 let isEven:boolean = false;
 const format:string = "D MMMM Y";
@@ -21,16 +23,49 @@ const MessageBox = (props:any):JSX.Element => {
             return message;
         }
     });
-    console.log('messages avec ownership', messages);
+    // console.log('messages avec ownership', messages);
 
     // groupedMessages est un objet (dictionaire)
     const groupedMessages:any = _.groupBy(messages, message => {
         return moment(message.createdAt).format(format);
     })
-    console.log('groupedMessages', groupedMessages);
+    // console.log('groupedMessages', groupedMessages);
+    const newMessages:any[] = Object.keys(groupedMessages)
+                                .map(key => {
+                                    return {
+                                        date: key,
+                                        groupedMessages: groupedMessages[key],
+                                        today: moment().format(format) === key
+                                    }
+                                });
+    // console.log('newMessages', newMessages);
+    const renderMessages = (newMessage:any):JSX.Element[] => {
+        return newMessage.groupedMessages.map(message => {
+            const msgClass:string = `message message--${message.ownership}`;
+            return (
+                <MessageText 
+                    key={message._id}
+                    msgClass={msgClass}
+                    content={message.content}
+                    ownership={message.ownership}
+                />
+            )
+        })
+    }
+    const renderDays = ():JSX.Element[] => {
+        return newMessages.map((newMessage, index:number) => {
+            const dateText:string = newMessage.today ? "Aujourd'hui" : newMessage.date;
+            return (
+                <div key={index}>
+                    <Day date={newMessage.date} />
+                    {renderMessages(newMessage)}
+                </div>
+            )
+        })
+    }
     return (
         <StyledMessageBox>
-            Message Box
+            {renderDays()}
         </StyledMessageBox>
     )
 }
